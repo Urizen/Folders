ed2d13769d683684445a0c8ce3c7304c91b1dbdc
Urizen <urizen@silver-rain.com>
2009-10-19 20:27:14 +0100

    Added the functionality to the second tab.

---
+++ Classes/SecondTabViewController.h
@@ -0,0 +1,16 @@
+//
+//  SecondTabViewController.h
+//  FolderTest
+//
+//  Created by Bob Dapaah on 19/10/2009.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SecondTabViewController : UIViewController {
+
+}
+
+@end
